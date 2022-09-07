7c941837929e6acc25afc0eff3708164361beb99
dogfoot <dogfoot_log@naver.com>
2022-09-07 23:24:36 +0900

    [220907] {}, Object 타입

--- second.ts
+++ second.ts
@@ -17,3 +17,17 @@ export {};
 
 // Promise -> Pending(비동기 실행 상태) -> Settled (이거 자체는 그냥 성공, 실패 없이 완료됐다는 상태) -> 성공하면 resolved, 실패하면 rejected
 // promises.then().catch() ==> then, catch 모두 settled. then은 resolved, catch는 rejected
+
+const x: {} = 'hello';
+const y: Object = 'hi';
+// 빈 객체 오브젝트 형이 아니라 이 둘은 모든 타입. (null, undefined 제외)
+// 실제 객체는 소문자 object
+const xx: object = { hello: 'world' }; // object 지양, interface, type, class
+const z: unknown = 'hi';
+
+//unknown = {} | null | undefined (4.8버전)
+if (z) {
+  z; // {}. 모든 값이 됨
+} else {
+  z; // undefined, null
+}
